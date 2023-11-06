f39d7a97ea12bb1b5c92832fa49393ff0cedd1de
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2023-11-06 18:01:11 +0000

    Added alt text keys/values to each object in the posts array

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const posts = [
         username: "vincey1853",
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
+        avatarAlt: "Vincent van Gogh in formal attire",
         post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
+        postAlt: "Self-portrait of Vincent van Gogh, a famous painting by the artist himself, capturing his unique style",
+        caption: "just took a few mushrooms lol",
         likes: 21
     },
     {
@@ -13,8 +15,10 @@ const posts = [
         username: "gus1819",
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
+        avatarAlt: "An aged photograph featuring Gustave Courbet, a bearded man, captured in black and white",
         post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
+        postAlt: "Self-portrait of Gustave Courbet, depicting a man with his head in his hands",
+        caption: "i'm feelin a bit stressed tbh",
         likes: 4
     },
         {
@@ -22,9 +26,10 @@ const posts = [
         username: "jd1735",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
+        avatarAlt: "Joseph Ducreux self-portrait of a man with finger to lips",
         post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        postAlt: "A self-portrait of Joseph Ducreux, depicting a man in a hat, gesturing with a pointed finger",
+        caption: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
-]
-
+];
